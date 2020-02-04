6212929c39b0f21fb4c99317fa89e4837244c634
Cesar Victorio <cbvictorio@gmail.com>
2020-02-03 22:04:19 -0800

    added styles on src folder to add css to Router component from reach router

---
+++ src/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+import { Router as RouterContainer } from '@reach/router'
+
+export const Router = styled(RouterContainer)`
+    height: 100vh;
+    box-sizing: border-box;
+    font-family: Helvetica;
+`
