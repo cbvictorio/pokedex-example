b982d81a8b8fe5e47ce41e567e4da553dc761acc
Cesar Victorio <cbvictorio@gmail.com>
2020-02-03 21:59:34 -0800

    added Provider component to connect redux to App

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import { render } from 'react-dom'
+import { Provider } from 'react-redux'
 import App from './App'
+import store from 'ReduxFiles'
 
 const container = document.getElementById('root')
 
-render(<App />, container)
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , container)
 
