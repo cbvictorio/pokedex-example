bd99128a43d396c363d7ec33fc8cd62599b72018
Cesar Victorio <cbvictorio@gmail.com>
2020-02-06 21:42:40 -0800

    added index files to export all reducers

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,9 @@
+import allPokemonsReducer from '@/redux/reducers/set-all-pokemons-reducer'
+import singlePokemonReducer from '@/redux/reducers/fetch-single-pokemon-reducer'
+import pokemonDetailsReducer from '@/redux/reducers/pokemon-details-reducer'
+
+export default {
+    allPokemonsReducer,
+    singlePokemonReducer,
+    pokemonDetailsReducer
+}
